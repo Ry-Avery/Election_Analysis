06d8ac49f3f3ed450aeed5bfbba23dbca006ea81
Ry Avery <axeguy255@gmail.com>
2022-02-27 07:15:06 -0800

    Finished code for PyPoll.py
    Read data from election_results.csv and wrote analysis into election_analysis.txt

--- PyPoll.py
+++ PyPoll.py
@@ -12,9 +12,13 @@ winning_candidate=''
 winning_count=0
 winning_percentage=0
 
+
 with open(read_file, 'r') as election_data:
+# Opens and reads from election_results.csv and then closes file
     file_reader=csv.reader(election_data)
     # Assigned variable to read data from election_results.csv
+    headers=next(file_reader)
+    #Skipped header row
     for row in file_reader:
         candidate_name=row[2]
         if candidate_name not in candidate_options:
@@ -25,21 +29,33 @@ with open(read_file, 'r') as election_data:
         total_votes+=1
     # Cycled through rows in election_results.csv
 
-for candidate_name in candidate_options:
-    votes=candidate_votes[candidate_name]
-    vote_percentage=float(votes)/float(total_votes)*100
-    print(f'{candidate_name}- {votes:,} votes, {vote_percentage:.1f}% of the total vote.')
-    if votes>winning_count:
-        winning_candidate=candidate_name
-        winning_count=votes
-        winning_percentage=vote_percentage
-    # If candidate has the most votes so far, assign them the winner
-# Looped through each candidates stats
-
-print(
-    f'\n'
-    f'{winning_candidate} won the election with {winning_count:,} votes of {total_votes:,} total votes({winning_percentage:.2f}%).'
-    f'\n')
-
-# with open(write_file, 'w') as election_analysis:
-#     election_analysis.write('')
+with open(write_file, 'w') as election_analysis:
+# Opens and writes from election_analysis.txt and then closes file
+    election_heading=(
+        f'\nElection Results\n'
+        f'-------------------------\n'
+        f'Total Votes: {total_votes:,}\n'
+        f'-------------------------\n')
+    print(election_heading,end='')
+    election_analysis.write(election_heading)
+    # Prints and writes election heading
+    for candidate_name in candidate_options:
+        votes=candidate_votes[candidate_name]
+        vote_percentage=float(votes)/float(total_votes)*100
+        candidate_results=(f'{candidate_name}- {votes:,} votes, {vote_percentage:.1f}% of the total vote.\n')
+        print(candidate_results)
+        election_analysis.write(candidate_results)
+        # Prints and writes candidate results
+        if votes>winning_count:
+            winning_candidate=candidate_name
+            winning_count=votes
+            winning_percentage=vote_percentage
+        # If candidate has the most votes so far, assign them the winner
+    # Looped through each candidates stats
+    canididate_winner=(
+         f'-------------------------\n'
+         f'{winning_candidate} won the election with {winning_count:,} votes of {total_votes:,} total votes({winning_percentage:.2f}%).\n'
+         f'-------------------------\n')
+    print(canididate_winner)
+    election_analysis.write(canididate_winner)
+    # Prints and writes election winner
